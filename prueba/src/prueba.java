6802d0b092867f593b3f7387044a38a01455bf7c
iker <imenpal2611@g.educaand.es>
2023-09-22 11:09:06 +0200

    hola1

--- prueba/src/prueba.java
+++ prueba/src/prueba.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class prueba {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("hola");
+        Scanner hola = new Scanner(System.in);
+
     }
 }
