80f63bea1f2eb7a24d8615485aec350e13e733ac
iker <imenpal2611@g.educaand.es>
2023-09-27 12:48:16 +0200

    ejerciciojava

---
+++ prueba/src/Miprimerproyecto.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Miprimerproyecto {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduce el tipo de vehóculo (coche, moto, camión)");
+        String tipovehiculo = sc.nextLine();
+
+        if (tipovehiculo.equals("coche")){
+            System.out.println("velocidad = 120km/h");
+        }
+        else if (tipovehiculo.equals("moto")){
+            System.out.println("velocidad = 90km/h");
+        }
+        else if (tipovehiculo.equals("camión")){
+            System.out.println("velocidad = 80km/h");
+        }
+        else{
+            System.out.println("No hay más vehiculos");
+        }
+    }
+}
--- prueba/src/prueba.java
+++ prueba/src/prueba.java
@@ -4,6 +4,5 @@ public class prueba {
     public static void main(String[] args) {
         System.out.println("hola");
         Scanner hola = new Scanner(System.in);
-
     }
 }
