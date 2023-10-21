79373bf1f16f03c76cd9104ab62c274f5a7f2ce2
iker <imenpal2611@g.educaand.es>
2023-10-21 17:59:57 +0200

    casa21oct

--- prueba/src/Boletin1_7/Ejercicio3.java
+++ prueba/src/Boletin1_7/Ejercicio3.java
@@ -6,48 +6,58 @@ import Boletin1_1.MientradaSalida;
 import java.util.Scanner;
 
 public class Ejercicio3 {
-    private static final  int M_VALUE = 1000;
-    private static final  int D_VALUE = 500;
-    private static final  int C_VALUE = 100;
-    private static final  int I_VALUE = 50;
-    private static final  int X_VALUE = 10;
-    private static final  int V_VALUE = 1;
+    final static int M_VALUE = 1000;
+    final static int D_VALUE = 500;
+    final static int C_VALUE = 100;
+    final static int L_VALUE = 50;
+    final static int X_VALUE = 10;
+    final static int V_VALUE = 5;
+    final static int I_VALUE = 1;
+    static Scanner tc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        String numRomano = MientradaSalida.leerString("Introduce un número en romano");
-        //numRomano = "DCXI"
+        System.out.println("Introduce un número romano");
+        String numR = tc.nextLine().toUpperCase();
+        int valD = 0;
+        int lastVal = 0;
 
-        int valorDecimal = 0;
-        int lastDigitValue = 0;
-        for (int i = 0; i < numRomano.length(); i++){
-            char digito = numRomano.charAt(i);
-            int digiValue = valuedf(digito);
+        for (int i = 0; i<numR.length();i++){
+            char digito = numR.charAt(i);
+            int digitVal = valor (digito);
 
-            if (i == 0){
-
-                lastDigitValue = digiValue;
-
-            }
-            else {
-                if (lastDigitValue >= digiValue) {
-                    valorDecimal += digiValue;
-                    lastDigitValue = digiValue;
-                } else {
-                    valorDecimal = valorDecimal + digiValue - (2 * lastDigitValue);
+            if (i==0){
+                lastVal = digitVal;
+            }else{
+                if (lastVal >=digitVal){
+                    valD = lastVal;
+                    lastVal = digitVal;
+                }else{
+                    valD += digitVal -lastVal;
+                    lastVal = digitVal;
                 }
             }
-            lastDigitValue = digiValue;
         }
+        System.out.printf("El valor en número romanos es %d", valD);
     }
-    public static  int valuedf(char c){
-        return  switch (c)
-        {
-            case 'M': yield  M_VALUE;
-            case 'D': yield  D_VALUE;
-            case 'C': yield  C_VALUE;
-            case 'X': yield X_VALUE;
-            case 'v': yield V_VALUE;
-            case 'I': yield I_VALUE;
+
+    public static int valor(char c) {
+        return switch (c) {
+            case 'M':
+                yield M_VALUE;
+            case 'D':
+                yield D_VALUE;
+            case 'C':
+                yield C_VALUE;
+            case 'L':
+                yield L_VALUE;
+            case 'X':
+                yield X_VALUE;
+            case 'V':
+                yield V_VALUE;
+            case 'I':
+                yield I_VALUE;
+            default:
+                yield -1;
         };
     }
 }
-
