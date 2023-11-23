7bbb7d25208e0120d8acc58f62888f5430bee8af
iker <imenpal2611@g.educaand.es>
2023-11-23 14:44:07 +0100

    juev23nov

---
+++ prueba/src/Boletin4_1/Rectangulo.java
@@ -0,0 +1,11 @@
+package Boletin4_1;
+
+public class Rectangulo {
+   private double longitud;
+   private double ancho;
+
+   public  Rectangulo(){
+       longitud = 1;
+       this.ancho = 1;
+   }
+}
