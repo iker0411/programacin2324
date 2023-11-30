44d4399b0a13f1340b307ba79e32622ee5be515e
iker <imenpal2611@g.educaand.es>
2023-11-30 14:38:27 +0100

    jueves30nov

--- prueba/src/Tema3/EjercicioAvion/Principal.java
+++ prueba/src/Tema3/EjercicioAvion/Principal.java
@@ -7,7 +7,7 @@ public class Principal {
         Deposito d = new Deposito(200000, 180000);
         Avion a = new Avion("Airbus", "446", 13, d);
 
-        a.addRevision(new Revision(LocalDateTime.now(), true, 6));
-        System.out.println(a);
+        a.addRevision(new Revision(LocalDateTime.of(2023, 10, 8, 12, 8), true, 6));
+        System.out.println(a.puedevolar(1500)? "Puede recorrer esa distancia" : "No puede volar esa distancia");
     }
 }
