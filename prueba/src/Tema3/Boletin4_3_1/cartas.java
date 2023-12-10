837e730107f2d449f6c16433dd0a2bde62cf8aef
iker <imenpal2611@g.educaand.es>
2023-12-10 20:07:27 +0100

    casa10diciem

---
+++ prueba/src/Tema3/Boletin4_3_1/cartas.java
@@ -0,0 +1,13 @@
+package Tema3.Boletin4_3_1;
+
+public class cartas {
+    private String palo;
+    private int num;
+
+    public cartas(String palo, int num) {
+        this.palo = palo;
+        this.num = num;
+    }
+
+}
+
